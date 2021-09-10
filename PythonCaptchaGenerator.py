80c70a9616ad8765efbbf3491da36218f2ed2bb8
ABHIJITH WARRIER <46685919+abhijithwarrier@users.noreply.github.com>
2021-09-10 17:07:02 +0530

    Update PythonCaptchaGenerator.py

--- PythonCaptchaGenerator.py
+++ PythonCaptchaGenerator.py
@@ -3,7 +3,7 @@
 # PYTHON GUI TO GENERATE CAPTCHA IMAGE USING THE captcha LIBRARY OF PYTHON
 
 # The captcha Library is capable of generating Audio and Image CAPTCHAs.
-#
+
 # The module can be installed using the command - pip install captcha
 
 # Importing the necessary packages
